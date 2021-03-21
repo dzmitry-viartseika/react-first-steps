5b14f3ef3c05fa65af3ee876c51c176156f5fd97
Dmitry Verteyko <verteyko1990@gmail.com>
2021-03-21 12:33:36 +0300

    styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Child from './components/Child/child'
 
 class App extends Component {
--- src/components/Child/child.js
+++ src/components/Child/child.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import './child.css'
+import classes from './child.module.scss'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
     const inputClasses  = [];
 
+    const style = {
+        border: '1px solid red',
+        boxShadow: '0 4px 5px 0 rgba(0,0,0, .14)',
+    }
+
     inputClasses.push(props.title !== '' ? 'green' : 'red');
 
+
     return (
-        <div className="child">
-            <div>
+        <div className={classes.child}>
+            <div style={style}>
                 Child: { props.title }
             </div>
             <h1>{ props.children }</h1>
@@ -17,7 +23,7 @@ export default props => {
                 type="text"
                 onChange={props.onChangeHeadline}
                 value={props.title}
-                className={inputClasses}
+                className={classes['child__input']}
             />
             <button onClick={props.onChangeTitle}>Click</button>
             <button onClick={props.onDeleteCar}>Delete Car</button>
